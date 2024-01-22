d2f447ebc524b000f57afe8386cb9a3942820718
Julian Dehm <j.dehm@liqd.net>
2024-01-22 15:35:17 +0100

    change wording and add german translation

--- src/accordionui.ts
+++ src/accordionui.ts
@@ -1,6 +1,7 @@
 import { Plugin } from 'ckeditor5/src/core';
 import { ButtonView } from 'ckeditor5/src/ui';
-
+// eslint-disable-next-line
+import { add } from '@ckeditor/ckeditor5-utils/src/translation-service';
 import ckeditor5Icon from '../theme/icons/ckeditor.svg';
 
 export default class AccordionUI extends Plugin {
@@ -9,6 +10,8 @@ export default class AccordionUI extends Plugin {
 	}
 
 	public init(): void {
+		// Add translations
+		add( 'de', { 'Insert accordion': 'Akkordeon einfügen' } );
 		const editor = this.editor;
 		const t = editor.t;
 
@@ -17,7 +20,7 @@ export default class AccordionUI extends Plugin {
 			const view = new ButtonView( locale );
 
 			view.set( {
-				label: t( 'Accordion' ),
+				label: t( 'Insert accordion' ),
 				icon: ckeditor5Icon,
 				tooltip: true
 			} );
